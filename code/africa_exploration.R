7864b427e3084d25fc4b9a8181c86652146f1fd9
traceybit <traceymangin@gmail.com>
2019-01-22 16:20:14 -0800

    who is fishing near/in these EEZs?

---
+++ code/africa_exploration.R
@@ -0,0 +1,10 @@
+## Tracey Mangin
+## January 22, 2018
+## Africa exploration
+
+library(janitor)
+library(tidyverse)
+
+boundary_gfw <- read_csv("~/Box/SFG Centralized Resources/Projects/cc_trade/data/full_dataset.csv")
+
+## countries of interest (West Africa)
